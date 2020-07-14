b200253ab16344f5853dd3e9266b19e1e6e58526
Pavel Janda <me@paveljanda.com>
2020-07-14 23:07:25 +0200

    Using di-nette

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use DI\Container;
 use Doctor\Rest\Application;
+use Nette\DI\Container;
 
 /**
  * @var Container
  */
 $container = require __DIR__ . '/../src/bootstrap.php';
 
-$container->get(Application::class)->run();
+$container->getByType(Application::class)->run();
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,15 +2,14 @@
 
 declare(strict_types=1);
 
-use Doctor\DI\PSR\Bootstrap;
+use Doctor\DI\Nette\Bootstrap;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$bootstrap = new Bootstrap;
+require __DIR__ . '/../vendor/doctor/di-nette/src/Bootstrap.php';
 
-//$bootstrap->setDebugMode(true);
+$bootstrap = new Bootstrap(__DIR__ . '/../cache', false);
 
-$bootstrap->addConfigFile(__DIR__ . '/../config/routes.php');
-$bootstrap->setCacheDir(__DIR__ . '/../cache');
+$bootstrap->addConfigFile(__DIR__ . '/../config/routes.neon');
 
 return $bootstrap->createContainer();
